1ca03b65f7d56f8467753ccc5b99c5ecbb32233d
MADEPADIO <madepadio99@gmail.com>
2025-08-20 21:35:40 +0100

    Completed Exercise 3

---
+++ input-processing-and-output/Printing-Quotes/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Printing_Quotes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the quote? ");
+            string quote = Console.ReadLine();
+            Console.Write("Who said it? ");
+            string author = Console.ReadLine();
+            Console.WriteLine($"{author} says, \"{quote}\"");
+        }
+    }
+}
