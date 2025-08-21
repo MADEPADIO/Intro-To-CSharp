693cdafd7273296e0a9ee16cd3de9e9d897b0f48
MADEPADIO <madepadio99@gmail.com>
2025-08-21 18:19:26 +0100

    Finished Exercise 6 and completed Chapter One of the book.

---
+++ input-processing-and-output/Retirement-Calculator-v2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Retirement_Calculator_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentAge, retirementAge, retirementYearsLeft;
+
+            currentAge = ConvertInputToNumber("What is your current age? ");
+            retirementAge = ConvertInputToNumber("What age would you like to retire? ");
+            retirementYearsLeft = retirementAge - currentAge;
+            if (retirementAge <= currentAge)
+                Console.WriteLine("You can retire already.");
+            else
+                Console.WriteLine($"The year is {DateTime.Now.Year}. You can retire in {DateTime.Now.Year + retirementYearsLeft}.");
+
+        }
+
+        public static int ConvertInputToNumber(string input)
+        {
+
+            bool isInputNumber = false;
+            string prompt;
+            int output;
+            do
+            {
+                Console.Write(input);
+                prompt = Console.ReadLine();
+                if (int.TryParse(prompt, out output) && output >= 0)
+                {
+                    isInputNumber = true;
+                    break;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Your input is supposed to be a positive number");
+                    isInputNumber = false;
+                }
+
+
+            }
+            while (isInputNumber == false);
+            return output;
+        }
+    }
+}
---
+++ input-processing-and-output/Retirement-Calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Retirement_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentAge, retirementAge, retirementYearsLeft;
+
+            currentAge = ConvertInputToNumber("What is your current age? ");
+            retirementAge = ConvertInputToNumber("What age would you like to retire? ");
+            retirementYearsLeft = retirementAge - currentAge;
+            Console.WriteLine($"The year is {DateTime.Now.Year}. You can retire in {DateTime.Now.Year + retirementYearsLeft}.");
+            
+        }
+
+        public static int ConvertInputToNumber(string input)
+        {
+
+            bool isInputNumber = false;
+            string prompt;
+            int output;
+            do
+            {
+                Console.Write(input);
+                prompt = Console.ReadLine();
+                if (int.TryParse(prompt, out output) && output >= 0)
+                {
+                    isInputNumber = true;
+                    break;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Your input is supposed to be a positive number");
+                    isInputNumber = false;
+                }
+
+
+            }
+            while (isInputNumber == false);
+            return output;
+        }
+    }
+}
