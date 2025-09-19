3f7ce45868881ad8bfdfa6ee63a1644a787f9f86
MADEPADIO <madepadio99@gmail.com>
2025-09-19 02:40:08 +0100

    Self Checkout version 1: Currently prompts for number of items and their prices, then sums them up with taxes to give final output.

---
+++ Chapter-03-calculations/Self-Checkout/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Self_Checkout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfItems = ConvertInputToNumber("How many items did you purchase: "), item = 1;
+            decimal pricesOfItems, subtotal = 0, total, tax;
+            
+
+            while (numberOfItems > 0)
+            {
+                pricesOfItems = ConvertInputToNumber($"What is the price of item {item}: ");
+                subtotal += pricesOfItems;
+                item++;
+                numberOfItems--;
+            }
+            tax = (5.5m / 100) * subtotal;
+            total = tax + subtotal;
+
+            Console.WriteLine($"Subtotal: {subtotal} \n Tax: {tax} \n Total: {total}");
+            
+        }
+
+        public static int ConvertInputToNumber(string input)
+        {
+
+            bool isInputNumber = false;
+            string prompt;
+            int output;
+            do
+            {
+                Console.Write(input);
+                prompt = Console.ReadLine();
+                if (int.TryParse(prompt, out output) && output >= 0)
+                {
+                    isInputNumber = true;
+                    break;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Your input is supposed to be a positive number");
+                    isInputNumber = false;
+                }
+
+
+            }
+            while (isInputNumber == false);
+            return output;
+        }
+    }
+}
