451187054c5dbd3da6266b09ff36c6ae04afdee6
MADEPADIO <madepadio99@gmail.com>
2025-09-19 02:48:13 +0100

    Self Checkout version 2: prompts for quantity of items bought.

---
+++ Chapter-03-calculations/Self-Checkout-v2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Self_Checkout_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfItems = ConvertInputToNumber("How many items did you purchase: "), item = 1, quantity;
+            decimal pricesOfItems, subtotal = 0, total, tax;
+
+
+            while (numberOfItems > 0)
+            {
+                pricesOfItems = ConvertInputToNumber($"What is the price of item {item}: ");
+                quantity = ConvertInputToNumber("Quantity: ");
+
+                subtotal += (quantity * pricesOfItems);
+                item++;
+                numberOfItems--;
+            }
+            tax = (5.5m / 100) * subtotal;
+            total = tax + subtotal;
+
+            Console.WriteLine($"Subtotal: {subtotal} \n Tax: {tax} \n Total: {total}");
+
+        }
+
+        public static int ConvertInputToNumber(string input)
+        {
+
+            bool isInputNumber = false;
+            string prompt;
+            int output;
+            do
+            {
+                Console.Write(input);
+                prompt = Console.ReadLine();
+                if (int.TryParse(prompt, out output) && output >= 0)
+                {
+                    isInputNumber = true;
+                    break;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Your input is supposed to be a positive number");
+                    isInputNumber = false;
+                }
+
+
+            }
+            while (isInputNumber == false);
+            return output;
+        }
+    }
+}
