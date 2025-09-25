1122c6abe818685d6bf9ac385e4dc4165a688c6e
MADEPADIO <madepadio99@gmail.com>
2025-09-25 03:11:33 +0100

    Calculates currency conversion using direct exchange rates formula.

---
+++ Chapter-03-calculations/Currency-Conversion/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Currency_Conversion
+{
+	internal class Program
+	{
+
+		static void Main(string[] args)
+		{
+			Console.Write("What currency are you converting from? (e.g., USD): ");
+			string fromCurrency = Console.ReadLine().ToUpper();
+			Console.Write("What currency are you converting to? (e.g., EUR): ");
+			string toCurrency = Console.ReadLine().ToUpper();
+			decimal amountToConvert = ConvertInputToNumber($"How many {fromCurrency} are you exchanging? "),
+					conversionRate = ConvertInputToNumber($"What is the conversion rate from {fromCurrency} to {toCurrency}? "),
+					convertedAmount;
+			convertedAmount = amountToConvert * conversionRate;
+			Console.WriteLine($"{amountToConvert} {fromCurrency} at a conversion rate of {conversionRate} is {convertedAmount} {toCurrency}.");
+
+
+
+        }
+
+		public static decimal ConvertInputToNumber(string input)
+		{
+
+			bool isInputNumber = false;
+			string prompt;
+			decimal output;
+			do
+			{
+				Console.Write(input);
+				prompt = Console.ReadLine();
+				if (decimal.TryParse(prompt, out output) && output >= 0)
+				{
+					isInputNumber = true;
+					break;
+
+				}
+
+				else
+				{
+					Console.WriteLine("Your input is supposed to be a positive number");
+					isInputNumber = false;
+				}
+
+
+			}
+			while (isInputNumber == false);
+			return output;
+		}
+	}
+}
