ec4710864e0b52fcf93b916eefe44d3b14a3e8d3
MADEPADIO <madepadio99@gmail.com>
2025-10-10 22:41:30 +0100

    Simple interest v1, had no big changes

---
+++ Chapter-03-calculations/Computing-Simple-Interest/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Computing_Simple_Interest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal principalAmount = ConvertInputToDecimal("How much are you investing? "),
+                    percent = ConvertInputToDecimal("How many percent? "),
+                    rate = percent / 100,
+                    Amount;
+            int time = ConvertInputToInteger("How many years are you investing? ");
+
+            Amount = principalAmount * (1 + (rate * time));
+            Console.WriteLine($"After {time} years at {percent}%, the investment will be worth ${Math.Round(Amount, 2, MidpointRounding.AwayFromZero)}");
+                    
+        }
+
+        public static decimal ConvertInputToDecimal(string input)
+        {
+
+            bool isInputNumber = false;
+            string prompt;
+            decimal output;
+            do
+            {
+                Console.Write(input);
+                prompt = Console.ReadLine();
+                if (decimal.TryParse(prompt, out output) && output >= 0)
+                {
+                    isInputNumber = true;
+                    break;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Your input is supposed to be a positive number");
+                    isInputNumber = false;
+                }
+
+
+            }
+            while (isInputNumber == false);
+            return output;
+        }
+
+        public static int ConvertInputToInteger(string input)
+        {
+
+            bool isInputNumber = false;
+            string prompt;
+            int output;
+            do
+            {
+                Console.Write(input);
+                prompt = Console.ReadLine();
+                if (int.TryParse(prompt, out output) && output >= 0)
+                {
+                    isInputNumber = true;
+                    break;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Your input is supposed to be a positive number");
+                    isInputNumber = false;
+                }
+
+
+            }
+            while (isInputNumber == false);
+            return output;
+        }
+
+        public static double ConvertInputToDouble(string input)
+        {
+            bool isInputNumber = false;
+            string prompt;
+            double output;
+            do
+            {
+                Console.Write(input);
+                prompt = Console.ReadLine();
+                if (double.TryParse(prompt, out output) && (double)output >= 0)
+                {
+                    isInputNumber = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Your input is supposed to be a positive number");
+                    isInputNumber = false;
+                }
+            }
+            while (isInputNumber == false);
+            return output;
+        }
+    }
+}
