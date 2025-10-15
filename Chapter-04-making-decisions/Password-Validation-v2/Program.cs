a87b5825f352096db60187fdc80fbe84040ee6e6
MADEPADIO <madepadio99@gmail.com>
2025-10-15 21:08:47 +0100

    Created a program that checks if a user password is correct.

---
+++ Chapter-04-making-decisions/Password-Validation-v2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Password_Validation_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string storedPassword = "!1234a.";
+            do
+            {
+                Console.Write("What is the password? ");
+                string passWord = ReadPassword();
+                if (passWord != storedPassword)
+                {
+                    Console.WriteLine("Wrong Password!");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Welcome Master!");
+                    break;
+                }
+
+            }
+            while (true);
+
+
+        }
+
+        public static string ReadPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey(intercept: true);
+                if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    password = password.Remove(password.Length - 1);
+                }
+                else if (!char.IsControl(key.KeyChar))
+                {
+                    password += key.KeyChar;
+                }
+            } while (key.Key != ConsoleKey.Enter);
+            Console.WriteLine();
+            return password;
+
+        
+        }
+    }
+}
---
+++ Chapter-04-making-decisions/Password-Validation/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Password_Validation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "!1234a.";
+            do
+            {
+                string passWord = ConvertInputToString("What is the password? ");
+                if (passWord != password)
+                {
+                    Console.WriteLine("Wrong Password!");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Welcome Master!");
+                    break;
+                }
+
+            }
+            while (true);
+
+           
+        }
+
+        public static string ConvertInputToString(string input)
+        {
+            bool isInputString = false;
+            string prompt;
+            string output = string.Empty;
+            do
+            {
+                Console.Write(input);
+                prompt = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(prompt))
+                {
+                    isInputString = true;
+                    output = prompt;
+                    break;
+                }
+
+
+            }
+            while (isInputString == false);
+            return output;
+        }
+    }
+}
