17b60bef9925ee5b3e53f40b94c0a711af8c30b0
MADEPADIO <madepadio99@gmail.com>
2025-10-16 19:16:54 +0100

    A basic driving age program that checks if you're legal enough to drive or not.

---
+++ Chapter-04-making-decisions/Legal-Driving-Age/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Legal_Driving_Age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = ConvertInputToNumber("How old are you? ");
+            Console.WriteLine(age >= 16 ? "You have reached the legal age to drive." : "You are not yet legally allowed to drive.");
+        }
+
+        public static int ConvertInputToNumber(string input)
+        {
+
+            bool isInputNumber = false;
+            string prompt;
+            int output;
+            do
+            {
+                Console.Write(input);
+                prompt = Console.ReadLine();
+                if (int.TryParse(prompt, out output) && output >= 0)
+                {
+                    isInputNumber = true;
+                    break;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Your input is supposed to be a positive number");
+                    isInputNumber = false;
+                }
+
+
+            }
+            while (isInputNumber == false);
+            return output;
+        }
+    }
+}
