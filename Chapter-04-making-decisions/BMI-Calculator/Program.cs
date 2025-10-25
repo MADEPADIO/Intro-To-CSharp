c44fe9779398022fd32008cb1714125271db2463
MADEPADIO <madepadio99@gmail.com>
2025-10-25 19:52:17 +0100

    Used a switch case for checking the number of months based on the users input.

---
+++ Chapter-04-making-decisions/BMI-Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace BMI_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight = ConvertInputToDouble("Enter you weight in kilograms: ");
+            double height = ConvertInputToDouble("Enter your height in centimetres: ") / 100;
+            var BMI = weight / (Math.Pow(height, 2));
+            Console.WriteLine(BMI < 18.5 ? $"Your BMI is {BMI}. You are underweight and should see your doctor." : BMI >= 25 ? $"Your BMI is {BMI}. You are overweight and should see your doctor." : $"Your BMI is {BMI}. You are within the ideal weight range.");
+        }
+
+        public static double ConvertInputToDouble(string input)
+        {
+            bool isInputNumber = false;
+            string prompt;
+            double output;
+            do
+            {
+                Console.Write(input);
+                prompt = Console.ReadLine();
+                if (double.TryParse(prompt, out output) && (double)output >= 0)
+                {
+                    isInputNumber = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Your input is supposed to be a positive number");
+                    isInputNumber = false;
+                }
+            }
+            while (isInputNumber == false);
+            return output;
+        }
+    }
+}
---
+++ Chapter-04-making-decisions/Number-To-Names/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Number_To_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfMonth = ConvertInputToNumber("Please enter a number of the month you want displayed: ");
+            do
+            {
+                switch (numberOfMonth)
+                {
+                    case 1:
+                        Console.WriteLine("The month is January");
+                        break;
+                    case 2:
+                        Console.WriteLine("The month is February");
+                        break;
+                    case 3:
+                        Console.WriteLine("The month is March");
+                        break;
+                    case 4:
+                        Console.WriteLine("The month is April");
+                        break;
+                    case 5:
+                        Console.WriteLine("The month is May");
+                        break;
+                    case 6:
+                        Console.WriteLine("The month is June");
+                        break;
+                    case 7:
+                        Console.WriteLine("The month is July");
+                        break;
+                    case 8:
+                        Console.WriteLine("The month is August");
+                        break;
+                    case 9:
+                        Console.WriteLine("The month is September");
+                        break;
+                    case 10:
+                        Console.WriteLine("The month is October");
+                        break;
+                    case 11:
+                        Console.WriteLine("The month is November");
+                        break;
+                    case 12:
+                        Console.WriteLine("The month is December");
+                        break;
+                    default:
+                        Console.WriteLine("This month does not exist");
+                        continue;
+                }
+            }
+            while (default == false);
+        }
+
+        public static int ConvertInputToNumber(string input)
+        {
+
+            bool isInputNumber = false;
+            string prompt;
+            int output;
+            do
+            {
+                Console.Write(input);
+                prompt = Console.ReadLine();
+                if (int.TryParse(prompt, out output) && output >= 0)
+                {
+                    isInputNumber = true;
+                    break;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Your input is supposed to be a positive number");
+                    isInputNumber = false;
+                }
+
+
+            }
+            while (isInputNumber == false);
+            return output;
+        }
+
+    }
+}
