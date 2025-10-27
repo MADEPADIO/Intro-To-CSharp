7e1bb3c7809683990cebc77eca3fc0d64a48e443
MADEPADIO <madepadio99@gmail.com>
2025-10-27 04:45:15 +0100

    Compared three numbers to determine the largest.

---
+++ Chapter-04-making-decisions/Comparing-Numbers/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Comparing_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, largest = 0;
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 3; i++)
+            {
+                numbers.Add(number = ConvertInputToNumber("Enter a number: "));
+            }
+            foreach (var num in numbers)
+            {
+                if (largest < num)
+                {
+                    largest = num;
+                }
+            }
+            Console.WriteLine($"The largest number is {largest}");
+        }
+
+        public static int ConvertInputToNumber(string input)
+        {
+
+            bool isInputNumber = false;
+            string prompt;
+            int output;
+            do
+            {
+                Console.Write(input);
+                prompt = Console.ReadLine();
+                if (int.TryParse(prompt, out output) && output >= 0)
+                {
+                    isInputNumber = true;
+                    break;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Your input is supposed to be a positive number");
+                    isInputNumber = false;
+                }
+
+
+            }
+            while (isInputNumber == false);
+            return output;
+        }
+    }
+}
